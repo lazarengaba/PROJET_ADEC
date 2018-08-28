f2d1286d584bcc18980a30d9bbdef2c0a7f1a881
TOGBA Lazare <ngabalazare3@gmail.com>
2018-08-28 09:54:33 +0000

    Creation de classes resolu

--- includedPages/menuBar.php
+++ includedPages/menuBar.php
@@ -1,6 +1,6 @@
 <div class="menuBar">
     <a href="./" class="indexBody">
-        <div class="" style="border: 1px solid #e44b1c; background-color: #e44b1c; color: #fff; width: 50px;">
+        <div class="" style="border: 1px solid #e44b1c; background-color: #e20a0a; color: #fff; width: 50px;">
             <i class="home icon"></i>
         </div>
     </a>
